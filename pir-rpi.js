3c6b39baca30cea2c0385e252a3307e54eeca206
RDSimmons01 <40708540+RDSimmons01@users.noreply.github.com>
2018-07-28 10:33:49 -0400

    Update pir-rpi.js

--- pir-rpi.js
+++ pir-rpi.js
@@ -1,14 +1,25 @@
-const Sensor = require('pi-pir-sensor');
-const sensor = new Sensor({
-    // pin number must be specified
-    pin: 11,
+const five = require("johnny-five");
+const board = new five.Board();
 
-    // loop time to check PIR sensor, defaults to 1.5 seconds
-    loop: 1500
+board.on("ready", function() {
+
+    // Create a new `motion` hardware instance.
+    const motion = new five.IR.Motion(7);
+
+    // "calibrated" occurs once, at the beginning of a session,
+    motion.on("calibrated", function() {
+        console.log("calibrated", Date.now());
+    });
+
+    // "motionstart" events are fired when the "calibrated"
+    // proximal area is disrupted, generally by some form of movement
+    motion.on("motionstart", function() {
+        console.log("motionstart", Date.now());
+    });
+
+    // "motionend" events are fired following a "motionstart" event
+    // when no movement has occurred in X ms
+    motion.on("motionend", function() {
+        console.log("motionend", Date.now());
+    });
 });
-
-sensor.on('movement', function () {
-    // who's there?
-});
-
-sensor.start();
